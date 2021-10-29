7af9bcc85d0faead5d5dd285e5ad3c702a2fae4e
AlcacovaTheBrabo <51960740+AlcacovaTheBrabo@users.noreply.github.com>
2021-10-29 20:23:50 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+    // Recebe os campos do formulário
+    $username = $_POST["username"];
+    $senha = $_POST["senha"];
+
+    // Realiza a consulta no banco de dados
+    include "conecta_mysql.inc";
+    $sql = "SELECT * FROM usuarios WHERE username = '$username';";
+    $res = mysqli_query($mysqli, $sql);
+
+    if(mysqli_num_rows($res) != 1){ // testa se não encontrou o username
+        echo "Username inválido!";
+        echo "<p><a href='login.html'>Página de login</a></p>";
+    }
+    else{
+        $usuario = mysqli_fetch_array($res);
+        if($senha != $usuario["senha"]){ // testa se a senha está errada
+            echo "Senha inválida!";
+            echo "<p><a href='login.html'>Página de login</a></p>";
+        }
+        else{ // usuário e senha corretos, abre a sessão
+            session_start();
+            $_SESSION["username"] = $username;
+            $_SESSION["senha"] = $senha;
+            // direciona à página inicial
+            header("Location: index.php");
+        }
+    }
+    mysqli_close($mysqli);
+?>
---
+++ recebe_dados.php
@@ -0,0 +1,47 @@
+<?php
+    $operacao = $_POST["operacao"];
+    include "conexao.php";
+
+    if($operacao == "inserir"){
+        $username = $_POST["username"];
+        $senha = $_POST["senha"];
+        $email = $_POST["email"];
+        $consenha = $_POST["confirmarsenha"];
+        $erro = 0;
+
+        if(strlen($username) < 5 OR strlen($username) > 10){
+            echo "O username deve possuir no mínimo 5 e no máximo 10 caracteres.<br>";
+            $erro = 1;
+        }
+        if(strlen($senha) < 5 OR strlen($senha) > 10){
+            echo "A senha deve possuir no mínimo 5 e no máximo 10 caracteres.<br>";
+            $erro = 1;
+        }
+        if($username == $senha){
+            echo "O username e a senha devem ser diferentes.<br>";
+            $erro = 1;
+        }
+        if(strlen($email) < 8 || strstr($email,'@') == FALSE){
+            echo "Favor digitar seu email corretamente. <br>";
+            $erro = 1;
+        }
+        if(strlen($email) > 30){
+            echo "O email deve possuir no máximo 30 caracteres.<br>";
+            $erro = 1;
+        }
+        if( $senha != $consenha );
+            echo "As senhas precisam ser iguais.";
+            $erro = 1;
+
+        // VERIFICA SE NÃO HOUVE ERRO 
+        if($erro == 0) {
+            $sql = "INSERT INTO usuarios (username,senha,email)";
+            $sql .= "VALUES ('$username','$senha','$email');";  
+            mysqli_query($mysqli,$sql);  
+            echo "<br>O usuário foi cadastrado com sucesso!"; 
+        }
+    }
+    
+    
+    mysqli_close($mysqli);
+?>
