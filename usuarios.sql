ee08a9e8d81964a9f79853d03f267aaba5ea52fb
AlcacovaTheBrabo <51960740+AlcacovaTheBrabo@users.noreply.github.com>
2021-11-24 16:30:48 -0300

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 23-Nov-2021 às 23:25
+-- Generation Time: 24-Nov-2021 às 20:27
 -- Versão do servidor: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -18,9 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE cookeat;
-USE cookeat;
-
 --
 -- Database: `cookeat`
 --
@@ -33,13 +30,15 @@ USE `cookeat`;
 -- Estrutura da tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `cod_usuario` int(11) NOT NULL,
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `cod_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(10) NOT NULL,
   `senha_cript` varchar(255) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `adm` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `adm` tinyint(1) NOT NULL,
+  PRIMARY KEY (`cod_usuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -50,26 +49,7 @@ INSERT INTO `usuarios` (`cod_usuario`, `username`, `senha_cript`, `email`, `adm`
 (22, 'olaria', '$2y$10$M39.GVdsLEp26LWBeFjY/OK/bH9i9EuvAMBT3cBE5uzX7HzN92iWO', 'rafhael.pimentel.rj@gmail.com', 0),
 (24, 'clecio', '$2y$10$54P2N91it/cK52Bmy4nBTOkiOBXPduwdGy5teOF7lo4l9hp2jCHIi', 'rafhael.pimentel.rj@gmail.com', 1),
 (25, 'JosÃ©', '$2y$10$OWM47RnDbzeLSgypfYaz0u1qETZCjxeiEjRZaohgYzzvwSFXGBBrC', 'rafhael.pimentel.rj@gmail.com', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`cod_usuario`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;COMMIT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
