33485b1629bc7fba73e203ab224b7b39a8c0731b
AlcacovaTheBrabo <51960740+AlcacovaTheBrabo@users.noreply.github.com>
2021-12-08 21:27:28 -0300

    Add files via upload

---
+++ src/cadastro.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulário</title>
+        <link rel="stylesheet" href="form.css">
+        <script src="https://kit.fontawesome.com/47e36a68f2.js" crossorigin="anonymous"></script>
+        <title>Cadastro</title>
+    </head>
+
+
+	
+    
+    <body>
+        <div id="form">
+            <form action="recebe_dados.php" method = "POST">
+                <input type="hidden" name="operacao" id="operacao" value="inserir">
+                <h2 class="title">Cadastrar</h2>
+                <label for="username">Username</label>
+                <div class="input">
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                    <input id="username" name="username" placeholder="Username" type="text">
+                </div>
+
+                <label for="email">Email</label>
+                <div class="input">
+                    <i class="far fa-envelope"></i>
+                    <input id="email" name="email" placeholder="Email" type="text">
+                </div>
+
+                <label for="senha">Senha</label>
+                <div class="input">
+                    <i class="fas fa-lock"></i>
+                    <input id="senha" name="senha" placeholder="Senha" type="password">
+                </div>
+                
+                <label for="confirmarsenha">Confirmar senha</label>
+                <div class="input">
+                    <i class="fas fa-lock"></i>
+                    <input id="confirmarsenha" name="confirmarsenha" placeholder="Senha" type="password">
+                </div>
+                
+                <div id="btn">
+                    <button type="submit">Cadastrar</button>
+                </div>
+            </form>
+        </div>   
+    </body>
+</html>
