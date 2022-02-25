82f0038602442a54eac40d1b6330f4a1c871b771
AlcacovaTheBrabo <51960740+AlcacovaTheBrabo@users.noreply.github.com>
2022-02-24 23:37:26 -0300

    Delete conteudo.php

--- src/conteudo.php
+++
@@ -1,8 +0,0 @@
-<?php
-    H@$pagina = $_GET['page']: "";
-        if($pagina == 2){
-            include "exibirpostagem.php";
-        } else if ($pagina == 3){
-            include "postagem.php";
-        }
-?>
